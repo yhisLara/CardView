9b2b02dc1506c7aa5018946c7485fdc29f3077fe
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-03-15 02:27:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/yhisl/cardview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yhisl.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewTitle = (TextView)findViewById(R.id.textViewTitle);
+        textViewTitle.setText("hello,CardView");
+    }
+}
